8c01cb4e6033629b155a1285e24a916fb7bfffbc
Jluo1030 <jyluo1030@outlook.com>
2024-10-25 19:51:11 +0800

    Add files via upload

---
+++ ex2/func/util.py
@@ -0,0 +1,50 @@
+import plotly.express as px
+import pandas as pd
+import os
+
+def clean_data(df):
+    # round the values for GDP to get rid of decimal points
+    df['GDP'] = df['GDP'].round()
+
+    return df
+
+def load_data(countries, folder_name):
+
+    dfs = []
+
+    for country in countries:
+        
+        file_name = f"{country}.csv"
+        file_path = os.path.join('.', folder_name, file_name)
+        
+        df = pd.read_csv(file_path)
+        df.columns = ['Date', 'GDP']
+        df['Country'] = country
+        
+        dfs.append(df)
+
+    combined_df = pd.concat(dfs)
+
+    combined_df['Date'] = pd.to_datetime(combined_df['Date'])
+
+    return combined_df
+
+def plot_data(df, countries):
+    filtered_df = df[df["Country"].isin(countries)]
+    fig = px.line(
+        filtered_df,
+        x="Date",
+        y="GDP",
+        color="Country",
+        title="2000-2022 GDP Data ",
+        labels={"GDP": "GDP (in current USD)", "Date": "Year"},
+        line_shape="linear",
+    )
+    fig.update_layout(
+        xaxis_title="Year",
+        yaxis_title="Nominal GDP, Index, Jan 2000 = 100",
+        legend_title="Country",
+        template="plotly_white"
+    )
+
+    fig.show()
